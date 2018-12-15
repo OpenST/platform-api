ee61f1403ed61961897f266a2dfb2f47ff08d92a
alpeshvmodi <alpeshvmodi@gmail.com>
2018-12-15 19:43:06 +0530

    config validations.

--- config/template.js
+++ config/template.js
@@ -6,7 +6,13 @@ const rootPrefix = '..',
 let configTemplate = {'entitiesMap': {}, 'rootLevelEntities': {}};
 
 configTemplate['rootLevelEntities'][configStrategyConstants.memcached] = "memcachedEntity";
+configTemplate['rootLevelEntities'][configStrategyConstants.nonceMemcached] = "memcachedEntity";
 configTemplate['rootLevelEntities'][configStrategyConstants.dynamodb] = "dynamodbEntity";
+configTemplate['rootLevelEntities'][configStrategyConstants.inMemoryCache] = "inMemoryCacheEntity";
+configTemplate['rootLevelEntities'][configStrategyConstants.originGeth] = "originGethEntity";
+configTemplate['rootLevelEntities'][configStrategyConstants.auxGeth] = "auxGethEntity";
+configTemplate['rootLevelEntities'][configStrategyConstants.elasticSearch] = "elasticSearchEntity";
+configTemplate['rootLevelEntities'][configStrategyConstants.rabbitmq] = "rabbitmqEntity";
 
 configTemplate['entitiesMap'] = {
 
@@ -35,6 +41,19 @@ configTemplate['entitiesMap'] = {
   consistentBehaviorEntity: {
     entityType: 'string'
   },
+  inMemoryCacheEntity: {
+    entityType: 'object',
+    entitiesPresent: {
+      engine: "engineEntity",
+      defaultTtl: "defaultTtlEntity",
+      namespace: "namespaceEntity",
+      consistentBehavior: "consistentBehaviorEntity"
+    }
+  },
+  namespaceEntity: {
+    entityType: 'string'
+  },
+
 
   dynamodbEntity: {
     entityType: 'object',
@@ -92,6 +111,106 @@ configTemplate['entitiesMap'] = {
       apiVersion: 'apiVersionEntity',
       enableSsl: 'enableSslEntity'
     }
+  },
+
+  originGethEntity: {
+    entityType: 'object',
+    entitiesPresent: {
+      rpcProvider: 'rpcProviderEntity',
+      rpcProviders: 'rpcProvidersEntity',
+      wsProvider: 'wsProviderEntity',
+      wsProviders: 'wsProvidersEntity',
+      chainId: 'chainIdEntity',
+      client: "gethClientEntity"
+    }
+  },
+  rpcProviderEntity: {
+    entityType: 'string'
+  },
+  rpcProvidersEntity: {
+    entityType: 'array',
+    entitiesPresent: 'rpcProviderEntity'
+  },
+  wsProviderEntity: {
+    entityType: 'string'
+  },
+  wsProvidersEntity: {
+    entityType: 'array',
+    entitiesPresent: 'wsProviderEntity'
+  },
+  chainIdEntity: {
+    entityType: 'number'
+  },
+  gethClientEntity: {
+    entityType: 'string'
+  },
+  auxGethEntity: {
+    entityType: 'object',
+    entitiesPresent: {
+      readOnly: 'auxGethProvidersEntity',
+      readWrite: "auxGethProvidersEntity",
+      chainId: 'chainIdEntity',
+      client: 'gethClientEntity'
+    }
+  },
+  auxGethProvidersEntity: {
+    entityType: 'object',
+    entitiesPresent: {
+      rpcProvider: 'rpcProviderEntity',
+      rpcProviders: 'rpcProvidersEntity',
+      wsProvider: 'wsProviderEntity',
+      wsProviders: 'wsProvidersEntity',
+    }
+  },
+
+  elasticSearchEntity: {
+    entityType: 'object',
+    entitiesPresent: {
+      host: 'hostEntity',
+      accessKey: 'accessKeyEntity',
+      region: 'regionEntity',
+      secretKey: 'secretKeyEntity'
+    }
+  },
+  hostEntity: {
+    entityType: 'string'
+  },
+  accessKeyEntity: {
+    entityType: 'string'
+  },
+  secretKeyEntity: {
+    entityType: 'string'
+  },
+
+  rabbitmqEntity: {
+    entityType: 'object',
+    entitiesPresent: {
+      username: 'usernameEntity',
+      password: 'passwordEntity',
+      host: 'hostEntity',
+      port: 'portEntity',
+      heartbeats: 'heartbeatsEntity',
+      clusterNodes: 'clusterNodesEntity'
+    }
+  },
+  usernameEntity: {
+    entityType: 'string'
+  },
+  passwordEntity: {
+    entityType: 'string'
+  },
+  portEntity: {
+    entityType: 'string'
+  },
+  heartbeatsEntity: {
+    entityType: 'string'
+  },
+  clusterNodesEntity:{
+    entityType: 'array',
+    entitiesPresent: 'clusterNodeEntity'
+  },
+  clusterNodeEntity: {
+    entityType: 'string'
   }
 
 };
