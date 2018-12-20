77418977699e9796919d095937d30e0fd5ecce1e
Puneet Khushwani <puneet.khushwani@ost.com>
2018-12-20 17:05:10 +0530

    generate key

---
+++ tools/helpers/GeneratePrivateKey.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * Generate private key
+ *
+ * @module tools/chainSetup/helpers/GeneratePrivateKey
+ */
+
+const rootPrefix = '../..',
+  Web3 = require('web3'),
+  responseHelper = require(rootPrefix + '/lib/formatter/response');
+
+class GenerateRawKeyKlass {
+  constructor() {}
+
+  /**
+   * Perform<br><br>
+   *
+   * @return {result}
+   */
+  perform() {
+    const web3Object = new Web3();
+    let newAddress = web3Object.eth.accounts.create(web3Object.utils.randomHex(32));
+
+    return responseHelper.successWithData({ address: newAddress.address, privateKey: newAddress.privateKey });
+  }
+}
+
+module.exports = GenerateRawKeyKlass;
