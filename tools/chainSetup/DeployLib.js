9b8945dedf45dfa5e615dbb7eb2ded4fdfb8d21f
Puneet Khushwani <puneet.khushwani@ost.com>
2019-01-01 22:33:36 +0530

    deploy libs steps

---
+++ tools/chainSetup/DeployLib.js
@@ -0,0 +1,294 @@
+'use strict';
+
+/**
+ * deploy lib
+ *
+ * @module tools/chainSetup/DeployLib
+ */
+const rootPrefix = '../..',
+  OSTBase = require('@openstfoundation/openst-base'),
+  InstanceComposer = OSTBase.InstanceComposer,
+  responseHelper = require(rootPrefix + '/lib/formatter/response'),
+  coreConstants = require(rootPrefix + '/config/coreConstants'),
+  logger = require(rootPrefix + '/lib/logger/customConsoleLogger'),
+  ConfigStrategyObject = require(rootPrefix + '/helpers/configStrategy/Object'),
+  ChainAddressModel = require(rootPrefix + '/app/models/mysql/ChainAddress'),
+  chainAddressConstants = require(rootPrefix + '/lib/globalConstant/chainAddress'),
+  ChainSetupLogsModel = require(rootPrefix + '/app/models/mysql/ChainSetupLogs'),
+  DeployLibs = require(rootPrefix + '/tools/commonSetup/DeployLibs'),
+  chainSetupLogsConstants = require(rootPrefix + '/lib/globalConstant/chainSetupLogs');
+
+/**
+ *
+ * @class
+ */
+class DeployLib {
+  /**
+   * Constructor
+   *
+   * @param {Object} params
+   * @param {String} params.chainKind - origin / aux (chain kind for which anchor org is to be setup)
+   * @param {String} params.libKind - merklePatriciaProof / messageBus / gateway
+   *
+   * @constructor
+   */
+  constructor(params) {
+    const oThis = this;
+
+    oThis.chainKind = params['chainKind'];
+    oThis.libKind = params['libKind'];
+
+    oThis.chainId = null;
+    oThis.gasPrice = null;
+    oThis.configStrategyObj = null;
+    oThis.merklePatriciaProofAddress = null;
+    oThis.stepKind = null;
+    oThis.chainAddressKind = null;
+  }
+
+  /**
+   *
+   * Perform
+   *
+   * @return {Promise<result>}
+   *
+   */
+  perform() {
+    const oThis = this;
+
+    return oThis._asyncPerform().catch(function(error) {
+      if (responseHelper.isCustomResult(error)) {
+        return error;
+      } else {
+        logger.error(`${__filename}::perform::catch`);
+        logger.error(error);
+        return responseHelper.error({
+          internal_error_identifier: 't_cs_dl_1',
+          api_error_identifier: 'unhandled_catch_response',
+          debug_options: {}
+        });
+      }
+    });
+  }
+
+  /***
+   *
+   * @private
+   *
+   * @return {Promise<result>}
+   */
+  async _asyncPerform() {
+    const oThis = this;
+
+    await oThis._initializeVars();
+
+    let params = {
+      chainId: oThis.chainId,
+      libKind: oThis.libKind,
+      signerAddress: await oThis._getDeployerAddr(),
+      chainEndpoint: oThis._configStrategyObject.chainWsProvider(oThis.chainId, 'readWrite'),
+      gasPrice: oThis.gasPrice,
+      merklePatriciaProofAddress: oThis.merklePatriciaProofAddress
+    };
+
+    let deployHelper = new DeployLibs(params);
+
+    let setupRsp = await deployHelper.perform();
+
+    setupRsp.debugOptions = {
+      inputParams: {},
+      transactionParams: params
+    };
+
+    await oThis._insertIntoChainSetupLogs(setupRsp);
+
+    await oThis._insertIntoChainAddress(setupRsp);
+
+    return setupRsp;
+  }
+
+  /***
+   *
+   * init vars on the basis of chain kind
+   *
+   * @private
+   */
+  async _initializeVars() {
+    const oThis = this;
+
+    switch (oThis.chainKind) {
+      case chainAddressConstants.originChainKind:
+        oThis.chainId = oThis._configStrategyObject.originChainId;
+        oThis.gasPrice = '0x3B9ACA00'; //TODO: Add dynamic gas logic here
+        break;
+      case chainAddressConstants.auxChainKind:
+        oThis.chainId = oThis._configStrategyObject.auxChainId;
+        oThis.gasPrice = '0x0';
+        break;
+      default:
+        throw `unsupported chainKind: ${oThis.chainKind}`;
+    }
+
+    switch (oThis.libKind) {
+      case 'merklePatriciaProof':
+        oThis.stepKind = chainSetupLogsConstants.deployMerklePatriciaProofLibStepKind;
+        oThis.chainAddressKind = chainAddressConstants.merklePatriciaProofLibKind;
+        break;
+      case 'messageBus':
+        oThis.stepKind = chainSetupLogsConstants.deployMessageBusLibKind;
+        oThis.chainAddressKind = chainAddressConstants.messageBusLibKind;
+        oThis.merklePatriciaProofAddress = await oThis._merklePatriciaProofAddr();
+        break;
+      case 'gateway':
+        oThis.stepKind = chainSetupLogsConstants.deployGatewayLibKind;
+        oThis.chainAddressKind = chainAddressConstants.gatewayLibKind;
+        oThis.merklePatriciaProofAddress = await oThis._merklePatriciaProofAddr();
+        break;
+      default:
+        throw `unsupported libLind ${oThis.libKind}`;
+    }
+  }
+
+  /***
+   *
+   * get deployer addr
+   *
+   * @private
+   *
+   * @return {Promise}
+   *
+   */
+  async _getDeployerAddr() {
+    const oThis = this;
+
+    let fetchAddrRsp = await new ChainAddressModel().fetchAddress({
+      chainId: oThis.chainId,
+      kind: chainAddressConstants.deployerKind,
+      chainKind: oThis.chainKind
+    });
+
+    if (!fetchAddrRsp.data.address) {
+      return Promise.reject(
+        responseHelper.error({
+          internal_error_identifier: 't_cs_dl_3',
+          api_error_identifier: 'something_went_wrong'
+        })
+      );
+    }
+
+    return fetchAddrRsp.data.address;
+  }
+
+  /***
+   *
+   * get org contract addr
+   *
+   * @private
+   *
+   * @return {Promise}
+   *
+   */
+  async _merklePatriciaProofAddr() {
+    const oThis = this;
+
+    let fetchAddrRsp = await new ChainAddressModel().fetchAddress({
+      chainId: oThis.chainId,
+      kind: chainAddressConstants.merklePatriciaProofLibKind,
+      chainKind: oThis.chainKind
+    });
+
+    if (!fetchAddrRsp.data.address) {
+      return Promise.reject(
+        responseHelper.error({
+          internal_error_identifier: 't_cs_dl_4',
+          api_error_identifier: 'something_went_wrong'
+        })
+      );
+    }
+
+    return fetchAddrRsp.data.address;
+  }
+
+  /**
+   * Insert entry into chain setup logs table.
+   *
+   * @private
+   *
+   * @param response
+   *
+   * @return {Promise<Result>}
+   */
+  async _insertIntoChainSetupLogs(response) {
+    const oThis = this;
+
+    let insertParams = {};
+
+    insertParams['chainId'] = oThis.chainId;
+    insertParams['chainKind'] = oThis.chainKind;
+    insertParams['stepKind'] = oThis.stepKind;
+    insertParams['debugParams'] = response.debugOptions;
+    insertParams['transactionHash'] = response.data.transactionHash;
+
+    if (response.isSuccess()) {
+      insertParams['status'] = chainSetupLogsConstants.successStatus;
+    } else {
+      insertParams['status'] = chainSetupLogsConstants.failureStatus;
+      insertParams['debugParams']['errorResponse'] = response.toHash();
+    }
+
+    await new ChainSetupLogsModel().insertRecord(insertParams);
+
+    return responseHelper.successWithData({});
+  }
+
+  /***
+   *
+   * insert anchor contract address into chain address
+   *
+   * @param {Result} response
+   *
+   * @return {Promise}
+   *
+   * @private
+   */
+  async _insertIntoChainAddress(response) {
+    const oThis = this;
+
+    if (response.isFailure()) return response;
+
+    await new ChainAddressModel().insertAddress({
+      address: response.data['contractAddress'],
+      chainId: oThis.chainId,
+      kind: oThis.chainAddressKind,
+      chainKind: oThis.chainKind
+    });
+  }
+
+  /***
+   *
+   * config strategy
+   *
+   * @return {object}
+   */
+  get _configStrategy() {
+    const oThis = this;
+    return oThis.ic().configStrategy;
+  }
+
+  /***
+   *
+   * object of config strategy klass
+   *
+   * @return {object}
+   */
+  get _configStrategyObject() {
+    const oThis = this;
+    if (oThis.configStrategyObj) return oThis.configStrategyObj;
+    oThis.configStrategyObj = new ConfigStrategyObject(oThis._configStrategy);
+    return oThis.configStrategyObj;
+  }
+}
+
+InstanceComposer.registerAsShadowableClass(DeployLib, coreConstants.icNameSpace, 'DeployLib');
+
+module.exports = DeployLib;
---
+++ tools/commonSetup/DeployLibs.js
@@ -0,0 +1,109 @@
+'use strict';
+
+/**
+ * deploy libs
+ *
+ * @module tools/commonSetup/DeployLibs
+ */
+const rootPrefix = '../..',
+  responseHelper = require(rootPrefix + '/lib/formatter/response'),
+  logger = require(rootPrefix + '/lib/logger/customConsoleLogger'),
+  CommonSetupBase = require(rootPrefix + '/tools/commonSetup/Base'),
+  MosaicTbd = require('mosaic-tbd');
+
+/**
+ *
+ * @class
+ */
+class DeployLibs extends CommonSetupBase {
+  /**
+   * Constructor
+   *
+   * @param {Object} params
+   * @param {String} params.libKind - type of lib to be deployed
+   * @param {Number} params.chainId - chain id on which this is to be performed
+   * @param {String} params.signerAddress - address who signs Tx
+   * @param {String} params.chainEndpoint - url to connect to chain
+   * @param {String} params.gasPrice -  gas price to use
+   * @param {String} [params.merklePatriciaProofAddress] - merklePatriciaProof lib address
+   *
+   * @constructor
+   */
+  constructor(params) {
+    super(params);
+    const oThis = this;
+    oThis.libKind = params['libKind'];
+    oThis.merklePatriciaProofAddress = params['merklePatriciaProofAddress'];
+  }
+
+  /**
+   *
+   * @return {Promise<result>}
+   *
+   */
+  async _asyncPerform() {
+    const oThis = this;
+
+    let signerKey = await oThis._fetchSignerKey();
+
+    oThis._addKeyToWallet(signerKey);
+
+    let deployRsp = await oThis
+      ._deployLib()
+      .then(function(txReceipt) {
+        return responseHelper.successWithData({
+          transactionReceipt: txReceipt,
+          transactionHash: txReceipt.transactionHash,
+          contractAddress: txReceipt.contractAddress
+        });
+      })
+      .catch(function(errorResponse) {
+        logger.error(errorResponse);
+        return responseHelper.error({
+          internal_error_identifier: 't_cs_c_dl_1',
+          api_error_identifier: 'unhandled_catch_response',
+          debug_options: {}
+        });
+      });
+
+    oThis._removeKeyFromWallet(signerKey);
+
+    return Promise.resolve(deployRsp);
+  }
+
+  /***
+   *
+   * @return {Promise<void>}
+   *
+   * @private
+   */
+  async _deployLib() {
+    const oThis = this;
+
+    let nonceRsp = await oThis._fetchNonce(),
+      txOptions = {
+        gasPrice: oThis.gasPrice,
+        from: oThis.signerAddress,
+        nonce: nonceRsp.data['nonce']
+      };
+
+    let libsHelperObj = new DeployLibs.LibsHelper(oThis._web3Instance);
+
+    switch (oThis.libKind) {
+      case 'merklePatriciaProof':
+        return libsHelperObj.deployMerklePatriciaProof(txOptions);
+      case 'messageBus':
+        return libsHelperObj.deployMessageBus(oThis.merklePatriciaProofAddress, txOptions);
+      case 'gateway':
+        return libsHelperObj.deployGatewayLib(oThis.merklePatriciaProofAddress, txOptions);
+      default:
+        throw `unsupported libLind ${oThis.libKind}`;
+    }
+  }
+
+  static get LibsHelper() {
+    return MosaicTbd.ChainSetup.LibsHelper;
+  }
+}
+
+module.exports = DeployLibs;
