9de2573aae07ab6c37b39bebf79b5bc424ca2aa5
Dhananjay8 <dhananjay@ost.com>
2019-01-07 19:37:12 +0530

    updated service manager

--- tools/localSetup/serviceManager.js
+++ tools/localSetup/serviceManager.js
@@ -8,7 +8,6 @@ const shellAsyncCmd = require('node-cmd'),
   shellSource = require('shell-source');
 
 const rootPrefix = '../..',
-  setupConfig = require(rootPrefix + '/tools/setup/config'),
   coreConstants = require(rootPrefix + '/config/coreConstants'),
   setupHelper = require(rootPrefix + '/tools/localSetup/helper'),
   logger = require(rootPrefix + '/lib/logger/customConsoleLogger'),
@@ -258,131 +257,6 @@ class ServiceManager {
 
     // Start geth nodes
     await oThis.startGeth(chainType, chainId, purpose);
-  }
-
-  /**
-   * Start executable script command
-   *
-   * @param {String} executablePath: relative path of the executable file
-   * @param {String/Number} chainId
-   *
-   * @return {String}
-   *
-   * @private
-   */
-  _startExecutableCommand(executablePath, chainId) {
-    // TODO: Executables path?
-    let logFilename = executablePath
-      .split(' ')[0]
-      .split('/')
-      .slice(-1)[0]
-      .split('.')[0];
-    return (
-      'node $OPENST_PLATFORM_PATH/' +
-      executablePath +
-      ' >> ' +
-      setupHelper.setupFolderAbsolutePath() +
-      '/' +
-      setupHelper.logsFolderFor(chainAddressConst.auxChainKind, chainId) +
-      '/executables-' +
-      logFilename +
-      '.log'
-    );
-  }
-
-  /**
-   * Start executable
-   *
-   * @param {String} executablePath: relative path of the executable file
-   * @param {String/Number} chainId
-   *
-   * @return {Promise}
-   */
-  startExecutable(executablePath, chainId) {
-    // TODO: env vars path?
-    const oThis = this,
-      envFilePath = setupHelper.setupFolderAbsolutePath() + '/' + setupConfig.env_vars_file;
-
-    return new Promise(function(onResolve) {
-      // source env
-      shellSource(envFilePath, function(err) {
-        if (err) {
-          throw err;
-        }
-
-        logger.info('* Starting executable:', executablePath);
-        const cmd = oThis._startExecutableCommand(executablePath, chainId);
-        logger.info(cmd);
-        shellAsyncCmd.run(cmd);
-
-        logger.info('* Waiting for 10 seconds for executable to start.');
-        setTimeout(function() {
-          onResolve(Promise.resolve());
-        }, 10000);
-      });
-    });
-  }
-
-  /**
-   * Post platform setup
-   */
-  postSetupSteps(chainType, chainId) {
-    const oThis = this;
-
-    let chainBinFolder = setupHelper.binFolderFor(chainType, chainId),
-      cmd = oThis._startGethCommand(chainType, chainId, ''),
-      gethRunScript = 'run-' + chainType + +'-' + chainId.toString() + '.sh';
-
-    fileManager.touch(chainBinFolder + '/' + gethRunScript, '#!/bin/sh');
-    fileManager.append(chainBinFolder + '/' + gethRunScript, cmd);
-    logger.info(
-      '* Start ' +
-        chainType +
-        '-' +
-        chainId.toString() +
-        ' chain: sh ' +
-        setupHelper.setupFolderAbsolutePath() +
-        '/' +
-        chainBinFolder +
-        '/' +
-        gethRunScript
-    );
-
-    // Generate executables
-    // TODO: Need to do this?
-    const intercomProcessIdentifiers = setupHelper.intercomProcessIdentifiers();
-    for (let i = 0; i < intercomProcessIdentifiers.length; i++) {
-      let intercomIdentifier = intercomProcessIdentifiers[i],
-        utilityChainBinFolder = setupHelper.binFolderFor(chainAddressConst.auxChainKind, chainId),
-        executablePath = 'executables/inter_comm/' + intercomIdentifier + '.js',
-        intercomProcessDataFile =
-          setupHelper.setupFolderAbsolutePath() +
-          '/' +
-          setupHelper.utilityChainDataFilesFolder() +
-          '/' +
-          intercomIdentifier +
-          '.data',
-        cmd = oThis._startExecutableCommand(
-          executablePath + ' ' + intercomProcessDataFile + ' ' + setupHelper.configStrategyUtilityFilePath()
-        ),
-        runScript = 'run-' + intercomIdentifier + '.sh';
-
-      fileManager.touch(utilityChainBinFolder + '/' + runScript, '#!/bin/sh');
-      logger.log("echo '" + cmd + "' >> " + utilityChainBinFolder + '/' + runScript);
-      shellAsyncCmd.run("echo '" + cmd + "' >> " + utilityChainBinFolder + '/' + runScript);
-      logger.info('* Start ' + intercomIdentifier + ' intercomm: sh ' + utilityChainBinFolder + '/' + runScript);
-    }
-
-    // Make Utility gas price to default after deployment
-    // TODO: Need to do this?
-    let cmdq =
-      'export ' +
-      setupConfig.chains.utility.gas_price.env_var +
-      '=' +
-      "'" +
-      setupConfig.chains.utility.gas_price.value +
-      "'";
-    fileManager.append(setupConfig.env_vars_file, cmd);
   }
 }
 
