73e674ac090175047354bcd8244ac1823572b76e
ankitsingh <ankit@ost.com>
2019-01-10 11:56:43 +0530

    branded token deployment.

---
+++ app/models/mysql/Tokens.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * This is model for Tokens table.
+ *
+ * @module app/models/mysql/Tokens
+ */
+const rootPrefix = '../../..',
+  util = require(rootPrefix + '/lib/util'),
+  coreConstants = require(rootPrefix + '/config/coreConstants'),
+  ModelBase = require(rootPrefix + '/app/models/mysql/Base');
+
+const dbName = 'kit_saas_' + coreConstants.subEnvironment + '_' + coreConstants.environment;
+
+class Tokens extends ModelBase {
+  constructor() {
+    super({ dbName: dbName });
+
+    const oThis = this;
+
+    oThis.tableName = 'tokens';
+  }
+}
+
+module.exports = Tokens;
