d3e900f3d264fdac0ae83a018e53d104e3b3691e
santhu210 <santhosh.nitdgp2015@gmail.com>
2019-01-11 14:47:36 +0530

    Helper changes

--- tools/verifiers/helper.js
+++ tools/verifiers/helper.js
@@ -17,6 +17,16 @@ class VerifierHelper {
     return deployedCode.indexOf(binSubStr) !== -1;
   }
 
+  async validateContract(contractAddress, contractName, web3) {
+    const oThis = this;
+
+    let deployedCode = await oThis._getCodeFor(contractAddress, web3);
+
+    let binCode = await oThis._getBinString(contractName);
+
+    return binCode == deployedCode;
+  }
+
   async getMosoicTbdContractObj(organizationName, contractAddress) {
     const oThis = this;
 
@@ -30,19 +40,29 @@ class VerifierHelper {
   }
 
   get organizationBinSubStr() {
-    return 'a165627a7a723058201162374aa572857d392ca775bb9c0c01465c5381ef34f50633f95487cddb78320029';
+    return 'aa572857d392ca775bb9c0c01465c5381ef34f50633f95487cddb78320029';
   }
 
-  get simpleTokenPrimeBinSubStr() {
-    return 'a165627a7a72305820918e707f474974c8b48c93181612a5056b419afb4286b833746af7f480b24e480029';
-  }
-
-  get anchorContractBinSubStr() {
-    return 'ea165627a7a723058206929121165b48f49d3826d89cdf8e0a5b4177cf58107961c603d2c1e5ac196c20029';
+  get organizationName() {
+    return 'MockOrganization';
   }
 
   static get AbiBinProviderHelper() {
     return MosaicTbd.AbiBinProvider;
+  }
+
+  async _getCodeFor(contractAddress, web3) {
+    const oThis = this;
+
+    let deployedCode = await web3.eth.getCode(contractAddress);
+
+    return deployedCode;
+  }
+
+  async _getBinString(contractName) {
+    const oThis = this;
+
+    let contractBin = await oThis.AbiBinProviderHelper().getBIN(contractName);
   }
 }
 
