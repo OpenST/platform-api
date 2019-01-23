f6efb818362df3568803e1d9ee2379bf8d0b90b3
Dhananjay8 <dhananjay@ost.com>
2019-01-23 19:49:08 +0530

    route for tokens

--- routes/v2/index.js
+++ routes/v2/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const rootPrefix = '../..',
   testRoutes = require(rootPrefix + '/routes/v2/test'),
-  tokenRoutes = require(rootPrefix + '/routes/v2/token');
+  tokenRoutes = require(rootPrefix + '/routes/v2/tokens');
 
 const router = express.Router();
 
