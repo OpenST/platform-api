c3428990c6bd310cba93507a179b7fd8503f50a5
Shlok Gilda <shlok@ost.com>
2019-01-25 19:24:02 +0530

    Added route for grant eth or ost.

--- routes/internal/token.js
+++ routes/internal/token.js
@@ -9,22 +9,30 @@ require(rootPrefix + '/app/services/token/StartMint');
 
 router.post('/deploy', function(req, res, next) {
   req.decodedParams.apiName = 'tokenDeployment';
+  req.decodedParams.configStrategyRequired = false;
 
-  Promise.resolve(routeHelper.perform(req, res, next, '/app/services/token/Deployment', 'r_it_2'));
+  Promise.resolve(routeHelper.perform(req, res, next, '/app/services/token/Deployment', 'r_it_1'));
 });
 
 router.post('/mint', function(req, res, next) {
   req.decodedParams.apiName = 'startMint';
   req.decodedParams.clientConfigStrategyRequired = true;
 
-  Promise.resolve(routeHelper.perform(req, res, next, 'StartMint', 'r_it_3'));
+  Promise.resolve(routeHelper.perform(req, res, next, 'StartMint', 'r_it_2'));
 });
 
 router.get('/mint-details', function(req, res, next) {
   req.decodedParams.apiName = 'mintDetails';
   req.decodedParams.configStrategyRequired = false;
 
-  Promise.resolve(routeHelper.perform(req, res, next, '/app/services/token/Mint', 'r_it_4'));
+  Promise.resolve(routeHelper.perform(req, res, next, '/app/services/token/Mint', 'r_it_3'));
+});
+
+router.get('/grant', function(req, res, next) {
+  req.decodedParams.apiName = 'grantEthOst';
+  req.decodedParams.configStrategyRequired = true;
+
+  Promise.resolve(routeHelper.perform(req, res, next, 'GrantEthOst', 'r_it_4'));
 });
 
 module.exports = router;
