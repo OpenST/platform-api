c2295500cab6bec4d80cc18ce4d1135235da824a
Shlok Gilda <shlok@ost.com>
2019-01-30 23:58:33 +0530

    Updated cron processes.

--- app/models/mysql/CronProcesses.js
+++ app/models/mysql/CronProcesses.js
@@ -19,7 +19,9 @@ const dbName = 'saas_' + coreConstants.subEnvironment + '_' + coreConstants.envi
     '5': cronProcessesConstant.workflowWorker,
     '6': cronProcessesConstant.updateRealtimeGasPrice,
     '7': cronProcessesConstant.fundEth,
-    '8': cronProcessesConstant.fundStPrime
+    '8': cronProcessesConstant.fundStPrime,
+    '9': cronProcessesConstant.fundByChainOwnerOriginChainSpecific,
+    '10': cronProcessesConstant.fundStPrimeAndEthByChainOwner
   },
   statuses = {
     '1': cronProcessesConstant.runningStatus,
