c0fe446c916828bd2741b90e54e6a4fa17d2aa46
Kedar Chandrayan <kedarchandrayan@gmail.com>
2019-02-01 17:38:21 +0530

    code review changes.

--- helpers/configStrategy/ByChainId.js
+++ helpers/configStrategy/ByChainId.js
@@ -8,7 +8,7 @@ const rootPrefix = '../..',
   ConfigStrategyModel = require(rootPrefix + '/app/models/mysql/ConfigStrategy'),
   configStrategyValidator = require(rootPrefix + '/lib/validators/configStrategy'),
   configStrategyConstants = require(rootPrefix + '/lib/globalConstant/configStrategy'),
-  ChainConfigStrategyCache = require(rootPrefix + '/lib/cacheManagement/shared/ChainConfigStrategyIds'),
+  ChainConfigStrategyCache = require(rootPrefix + '/lib/cacheManagement/shared/ChainConfigStrategyId'),
   ConfigStrategyCache = require(rootPrefix + '/lib/sharedCacheMultiManagement/configStrategy');
 
 const errorConfig = basicHelper.fetchErrorConfig(apiVersions.general);
