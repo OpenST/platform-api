1b535e61973cd0f264a8f270c06c16564c5b4d38
Ashutosh <ashutosh@ost.com>
2019-02-21 22:56:52 +0530

    minor test update

--- tests/ledgerApis.js
+++ tests/ledgerApis.js
@@ -6,47 +6,45 @@ const OSTBase = require('@openstfoundation/openst-base'),
 const rootPrefix = '..';
 
 const StrategyByChainHelper = require(rootPrefix + '/helpers/configStrategy/ByChainId');
-const strategyByChainHelperObj = new StrategyByChainHelper(2000),
-  configStrategyResp = getStrategyByChainHelperObj();
+const strategyByChainHelperObj = new StrategyByChainHelper(2000);
 
-const configStrategy = configStrategyResp.data;
+require(rootPrefix + '/app/services/transaction/GetUserTransactions');
 
-const ic = new InstanceComposer(configStrategy);
+let configStrategyResp = null;
+getStrategyByChainHelperObj().then(function(res) {
+  configStrategyResp = res;
+  const configStrategy = configStrategyResp.data;
+  const ic = new InstanceComposer(configStrategy);
+  const coreConstants = require(rootPrefix + '/config/coreConstants'),
+    GetUserTransactions = ic.getShadowedClassFor(coreConstants.icNameSpace, 'GetUserTransactions');
+  let timestamp = parseInt(Date.now() / 100),
+    userId = 'id' + timestamp;
+
+  let userTransaction = new GetUserTransactions({
+    user_id: userId,
+    status: [0, 1, 2],
+    meta_property: [
+      {
+        n: 'name1',
+        t: 'type1',
+        d: 'details1'
+      },
+      {
+        n: 'name2',
+        t: 'type2',
+        d: 'details2'
+      }
+    ]
+  });
+
+  console.log('======START=====');
+  console.log('getServiceConfig', userTransaction.getServiceConfig());
+  console.log('getElasticSearchQuery', userTransaction.getElasticSearchQuery(userId));
+  console.log('======END=====');
+});
 
 async function getStrategyByChainHelperObj() {
   await strategyByChainHelperObj.getComplete();
 }
-
-require(rootPrefix + '/app/services/transaction/GetUserTransactions');
-
-const coreConstants = require(rootPrefix + '/config/coreConstants'),
-  GetUserTransactions = ic.getShadowedClassFor(coreConstants.icNameSpace, 'GetUserTransactions');
-
-console.log('GetUserTransactions', GetUserTransactions);
-
-let timestamp = parseInt(Date.now() / 100),
-  userId = 'id' + timestamp;
-
-let userTransaction = new GetUserTransactions({
-  user_id: userId,
-  status: [0, 1, 2],
-  meta_property: [
-    {
-      n: 'name1',
-      t: 'type1',
-      d: 'details1'
-    },
-    {
-      n: 'name2',
-      t: 'type2',
-      d: 'details2'
-    }
-  ]
-});
-
-console.log('======START=====');
-console.log('getServiceConfig', userTransaction.getServiceConfig());
-console.log('getElasticSearchQuery', userTransaction.getElasticSearchQuery(userId));
-console.log('======END=====');
 
 module.exports = {};
