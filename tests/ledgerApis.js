4862edfa3cb86b527288b40c1ab34f1f64908afb
Ashutosh <ashutosh@ost.com>
2019-02-21 22:49:00 +0530

    Temp

---
+++ tests/ledgerApis.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const OSTBase = require('@openstfoundation/openst-base'),
+  InstanceComposer = OSTBase.InstanceComposer;
+
+const rootPrefix = '..';
+
+const StrategyByChainHelper = require(rootPrefix + '/helpers/configStrategy/ByChainId');
+const strategyByChainHelperObj = new StrategyByChainHelper(2000),
+  configStrategyResp = getStrategyByChainHelperObj();
+
+const configStrategy = configStrategyResp.data;
+
+const ic = new InstanceComposer(configStrategy);
+
+async function getStrategyByChainHelperObj() {
+  await strategyByChainHelperObj.getComplete();
+}
+
+console.log('InstanceComposer', InstanceComposer);
+
+require(rootPrefix + '/app/services/transaction/GetUserTransactions');
+
+const coreConstants = require(rootPrefix + '/config/coreConstants'),
+  GetUserTransactions = ic.getShadowedClassFor(coreConstants.icNameSpace, 'GetUserTransactions');
+
+console.log('GetUserTransactions', GetUserTransactions);
+
+let timestamp = parseInt(Date.now() / 100),
+  userId = 'id' + timestamp;
+
+let userTransaction = new GetUserTransactions({
+  user_id: userId,
+  status: [0, 1, 2],
+  meta_property: [
+    {
+      n: 'name1',
+      t: 'type1',
+      d: 'details1'
+    },
+    {
+      n: 'name2',
+      t: 'type2',
+      d: 'details2'
+    }
+  ]
+});
+
+console.log('getServiceConfig', userTransaction.getServiceConfig());
+console.log('getElasticSearchQuery', userTransaction.getElasticSearchQuery(userId));
+
+console.log('======END=====');
+
+module.exports = {};
