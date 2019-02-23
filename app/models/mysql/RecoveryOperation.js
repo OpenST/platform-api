0c58d0296c982ed8dbd8ab81133756bfeeb6fa5b
Shlok Gilda <shlok@ost.com>
2019-02-23 13:25:53 +0530

    Updated recovery constants and model.

--- app/models/mysql/RecoveryOperation.js
+++ app/models/mysql/RecoveryOperation.js
@@ -5,28 +5,12 @@
  * @module /app/models/mysql/RecoveryOperation
  */
 const rootPrefix = '../../..',
-  util = require(rootPrefix + '/lib/util'),
   ModelBase = require(rootPrefix + '/app/models/mysql/Base'),
   coreConstants = require(rootPrefix + '/config/coreConstants'),
   recoveryOperationConstants = require(rootPrefix + '/lib/globalConstant/recoveryOperation');
 
 // Declare variables.
-const dbName = 'saas_big_' + coreConstants.subEnvironment + '_' + coreConstants.environment,
-  statuses = {
-    '1': recoveryOperationConstants.inProgressStatus,
-    '2': recoveryOperationConstants.waitingForAdminActionStatus,
-    '3': recoveryOperationConstants.completedStatus,
-    '4': recoveryOperationConstants.failedStatus
-  },
-  kinds = {
-    '1': recoveryOperationConstants.initiateRecoveryByUserKind,
-    '2': recoveryOperationConstants.abortRecoveryByUserKind,
-    '3': recoveryOperationConstants.pinResetByUserKind,
-    '4': recoveryOperationConstants.executeRecoveryByControllerKind,
-    '5': recoveryOperationConstants.abortRecoveryByControllerKind
-  },
-  invertedStatuses = util.invert(statuses),
-  invertedKinds = util.invert(kinds);
+const dbName = 'saas_big_' + coreConstants.subEnvironment + '_' + coreConstants.environment;
 
 /**
  * Class for RecoveryOperation model.
@@ -49,39 +33,24 @@ class RecoveryOperation extends ModelBase {
     oThis.tableName = 'recovery_operations';
   }
 
-  get statuses() {
-    return statuses;
-  }
-
-  get kinds() {
-    return kinds;
-  }
-
-  get invertedStatuses() {
-    return invertedStatuses;
-  }
-
-  get invertedKinds() {
-    return invertedKinds;
-  }
-
   /**
    * Get by Token Uer
    *
-   * @param token_id
-   * @param userId
+   * @param {String/Number} tokenId
+   * @param {String/Number} userId
+   *
    * @returns {*|void}
    */
-  getPendingOperationsOfTokenUser(token_id, userId) {
+  getPendingOperationsOfTokenUser(tokenId, userId) {
     const oThis = this,
       pendingStatuses = [
-        oThis.invertedStatuses[recoveryOperationConstants.inProgressStatus],
-        oThis.invertedStatuses[recoveryOperationConstants.waitingForAdminActionStatus]
+        recoveryOperationConstants.invertedStatuses[recoveryOperationConstants.inProgressStatus],
+        recoveryOperationConstants.invertedStatuses[recoveryOperationConstants.waitingForAdminActionStatus]
       ];
 
     return oThis
       .select('*')
-      .where(['token_id=? AND user_id=? AND status IN (?)', token_id, userId, pendingStatuses])
+      .where(['token_id=? AND user_id=? AND status IN (?)', tokenId, userId, pendingStatuses])
       .fire();
   }
 }
