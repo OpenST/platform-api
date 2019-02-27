db578486460747fd5a0b951cbba02425186228cb
aman-barbaria <amanbarbaria@gmail.com>
2019-02-27 13:06:19 +0530

    fix for api name authentication

--- routes/helper.js
+++ routes/helper.js
@@ -9,6 +9,7 @@ const rootPrefix = '..',
   logger = require(rootPrefix + '/lib/logger/customConsoleLogger'),
   responseHelper = require(rootPrefix + '/lib/formatter/response'),
   ApiParamsValidator = require(rootPrefix + '/lib/validators/ApiParams'),
+  apiName = require(rootPrefix + '/lib/globalConstant/apiName'),
   ConfigCrudByClientId = require(rootPrefix + '/helpers/configStrategy/ByClientId');
 
 class RoutesHelper {
@@ -64,7 +65,7 @@ class RoutesHelper {
       errorConfig = basicHelper.fetchErrorConfig(req.decodedParams.apiVersion);
 
     if (
-      req.decodedParams.app_validated_api_name &&
+      req.decodedParams.app_validated_api_name != apiName.allInternalRoutes &&
       req.decodedParams.app_validated_api_name != req.decodedParams.apiName
     ) {
       return responseHelper
