4d6d7281111d8c410033101369e28d1786e95c49
Kedar Chandrayan <kedarchandrayan@gmail.com>
2019-03-12 13:27:58 +0530

    code review comments.

--- app/models/mysql/Base.js
+++ app/models/mysql/Base.js
@@ -1,11 +1,19 @@
 'use strict';
-
+/**
+ * @file - Base file for all the MySQL Models
+ */
 const rootPrefix = '../../..',
   MysqlQueryBuilders = require(rootPrefix + '/lib/queryBuilders/mysql'),
   mysqlWrapper = require(rootPrefix + '/lib/mysqlWrapper'),
   logger = require(rootPrefix + '/lib/logger/customConsoleLogger');
 
 class ModelBase extends MysqlQueryBuilders {
+  /**
+   * Base Model Constructor
+   *
+   * @constructor
+   * @param params
+   */
   constructor(params) {
     super(params);
 
@@ -15,16 +23,31 @@ class ModelBase extends MysqlQueryBuilders {
     oThis.bitColumns = {};
   }
 
-  // get read connection
+  /**
+   * Connection pool to use for read query
+   *
+   * @return {*}
+   */
   onReadConnection() {
+    // At present, following is not being used. But when we implement replication,
+    // following connection pool will be used for slave connections.
     return mysqlWrapper.getPoolFor(this.dbName, 'master');
   }
 
-  // on write connection
+  /**
+   * Connection pool to use for write query
+   *
+   * @return {*}
+   */
   onWriteConnection() {
     return mysqlWrapper.getPoolFor(this.dbName, 'master');
   }
 
+  /**
+   * Fire the query
+   *
+   * @return {Promise<any>}
+   */
   fire() {
     const oThis = this;
 
@@ -43,122 +66,6 @@ class ModelBase extends MysqlQueryBuilders {
           }
         });
     });
-  }
-
-  /**
-   * Set Bit in the column for a given bit string.
-   *
-   * @param bitName
-   * @param previousValue
-   * @return {Number}
-   */
-  setBit(bitName, previousValue) {
-    const oThis = this;
-
-    let resp = oThis.findValueOfBit(bitName);
-    if (resp) {
-      logger.debug(resp);
-      return resp['bitValue'] | previousValue;
-    }
-
-    // If Bit is not found for any column then return old value.
-    return previousValue;
-  }
-
-  /**
-   * Unset Bit in the column for a given bit string.
-   *
-   * @param bitName
-   * @param previousValue
-   * @return {Number}
-   */
-  unsetBit(bitName, previousValue) {
-    const oThis = this;
-
-    let resp = oThis.findValueOfBit(bitName);
-    if (resp) {
-      let val = resp['bitValue'] ^ previousValue;
-      // If XOR operator returns value less than previous value means bit was set previously else not.
-      if (val < previousValue) {
-        return val;
-      }
-    }
-
-    // If Bit is not found for any column then return old value.
-    return previousValue;
-  }
-
-  /**
-   * Check whether bit is set in a value for a given bitName
-   *
-   * @param bitName
-   * @param currentValue
-   * @return {boolean}
-   */
-  isBitSet(bitName, currentValue) {
-    const oThis = this;
-
-    let resp = oThis.findValueOfBit(bitName);
-    if (resp) {
-      let val = resp['bitValue'] & currentValue;
-      // If Bitwise and operator returns 0 means bit is not set.
-      return val == resp['bitValue'];
-    }
-
-    // If Bit is not found for any column then return false.
-    return false;
-  }
-
-  /**
-   * Get all bits set for a given column
-   *
-   * @param columnName
-   * @param currentValue
-   * @return {Array}
-   */
-  getAllBits(columnName, currentValue) {
-    const oThis = this;
-
-    let allBits = oThis.bitColumns[columnName];
-    if (allBits && Object.keys(allBits).length > 0) {
-      let bitNames = Object.keys(allBits);
-      let arr = [];
-      for (let i = 0; i < bitNames.length; i++) {
-        if (oThis.isBitSet(bitNames[i], currentValue)) {
-          arr.push(bitNames[i]);
-        }
-      }
-      return arr;
-    }
-    return [];
-  }
-
-  /**
-   * Fetch Column and Value of a given bit
-   *
-   * @param bitName
-   * @return {*}
-   */
-  findValueOfBit(bitName) {
-    const oThis = this;
-
-    for (let columnName in oThis.bitColumns) {
-      let bitNameTobitValueMap = oThis.bitColumns[columnName];
-
-      if (bitNameTobitValueMap[bitName]) {
-        let bitValue = bitNameTobitValueMap[bitName];
-        return { column: columnName, bitValue: bitValue };
-      }
-    }
-
-    return null;
-  }
-
-  /**
-   * Set all data as hashmap for performing bitwise operations
-   */
-  setBitColumns() {
-    throw 'SubClass to Implement';
   }
 }
 
