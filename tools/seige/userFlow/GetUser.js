e1347666925cfca50b93d8140ff1415c7800debe
ankitsingh <ankit@ost.com>
2019-03-18 19:24:54 +0530

    loggers in siege scripts 2

--- tools/seige/userFlow/GetUser.js
+++ tools/seige/userFlow/GetUser.js
@@ -17,15 +17,19 @@ class GetUser {
       userService = oThis.ostObj.services.users,
       userdata = await userService.get({ user_id: oThis.userUuid }).catch(function(err) {
         console.log('User Data Error: ', oThis.userUuid, JSON.stringify(err));
-        return responseHelper.error({
-          internal_error_identifier: 't_s_uf_gu_1',
-          api_error_identifier: 'something_went_wrong',
-          debug_options: { API: 'GetUser' }
-        });
+        return {};
       });
 
-    console.log('====userdata', userdata);
-    return responseHelper.successWithData(userdata.data);
+    if (userdata['success']) {
+      return responseHelper.successWithData(userdata.data);
+    } else {
+      console.log('Error in api call', userdata);
+      return responseHelper.error({
+        internal_error_identifier: 't_s_uf_gu_1',
+        api_error_identifier: 'something_went_wrong',
+        debug_options: { API: 'GetUser' }
+      });
+    }
   }
 }
 
