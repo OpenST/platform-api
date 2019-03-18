a562c8ec279f52719b7f02b859855f431fccc22b
Shlok Gilda <shlok@ost.com>
2019-03-18 12:28:49 +0530

    Added alert framework usage.

--- executables/funding/byTokenAuxFunder/auxChainSpecific.js
+++ executables/funding/byTokenAuxFunder/auxChainSpecific.js
@@ -12,13 +12,14 @@ const program = require('commander');
 const rootPrefix = '../../..',
   GetStPrimeBalance = require(rootPrefix + '/lib/getBalance/StPrime'),
   TokenAddressModel = require(rootPrefix + '/app/models/mysql/TokenAddress'),
+  ErrorLogsConstants = require(rootPrefix + '/lib/errorLogs/ErrorLogsConstants'),
   TransferStPrimeBatch = require(rootPrefix + '/lib/fund/stPrime/BatchTransfer'),
   ByTokenAuxFunderBase = require(rootPrefix + '/executables/funding/byTokenAuxFunder/Base'),
   basicHelper = require(rootPrefix + '/helpers/basic'),
-  emailNotifier = require(rootPrefix + '/lib/notifier'),
   fundingAmounts = require(rootPrefix + '/config/funding'),
   logger = require(rootPrefix + '/lib/logger/customConsoleLogger'),
   responseHelper = require(rootPrefix + '/lib/formatter/response'),
+  createErrorLogsEntry = require(rootPrefix + '/lib/errorLogs/createEntry'),
   tokenAddressConstants = require(rootPrefix + '/lib/globalConstant/tokenAddress'),
   cronProcessesConstants = require(rootPrefix + '/lib/globalConstant/cronProcesses');
 
@@ -282,12 +283,14 @@ class FundByChainOwnerAuxChainSpecific extends ByTokenAuxFunderBase {
           logger.warn('addressKind tokenAuxFunder has low balance on chainId: ' + oThis.auxChainId);
           logger.warn('Address: ' + tokenAuxFunderAddress);
           logger.warn('TokenId: ' + tokenId);
-          await emailNotifier.perform(
-            'e_f_btaf_acs_1',
-            `Low balance of addressKind tokenAuxFunder for tokenId: ${tokenId}, Address: ${tokenAuxFunderAddress}`,
-            {},
-            {}
-          );
+
+          const errorObject = responseHelper.error({
+            internal_error_identifier: 'low_balance:e_f_btaf_acs_1',
+            api_error_identifier: 'low_balance',
+            debug_options: { tokenId: tokenId, address: tokenAuxFunderAddress }
+          });
+
+          await createErrorLogsEntry.perform(errorObject, ErrorLogsConstants.highSeverity);
 
           return responseHelper.error({
             internal_error_identifier: 'e_f_btaf_acs_2',
