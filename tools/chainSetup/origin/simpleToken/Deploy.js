2cdaad2a47fbde13773210b4aeccbdee296df232
Shlok Gilda <shlok@ost.com>
2019-04-10 14:51:28 +0530

    Comment changes.

--- tools/chainSetup/origin/simpleToken/Deploy.js
+++ tools/chainSetup/origin/simpleToken/Deploy.js
@@ -1,4 +1,3 @@
-'use strict';
 /**
  * Deploy simpleToken
  *
@@ -11,27 +10,31 @@ const OSTBase = require('@ostdotcom/base'),
 const rootPrefix = '../../../..',
   CoreAbis = require(rootPrefix + '/config/CoreAbis'),
   CoreBins = require(rootPrefix + '/config/CoreBins'),
-  logger = require(rootPrefix + '/lib/logger/customConsoleLogger'),
-  SetupSimpleTokenBase = require(rootPrefix + '/tools/chainSetup/origin/simpleToken/Base'),
-  chainAddressConstants = require(rootPrefix + '/lib/globalConstant/chainAddress'),
-  chainSetupConstants = require(rootPrefix + '/lib/globalConstant/chainSetupLogs'),
-  ChainAddressModel = require(rootPrefix + '/app/models/mysql/ChainAddress'),
-  coreConstants = require(rootPrefix + '/config/coreConstants'),
   DeployerKlass = require(rootPrefix + '/tools/helpers/Deploy'),
+  ChainAddressModel = require(rootPrefix + '/app/models/mysql/ChainAddress'),
+  UpdateStakeCurrenciesTable = require(rootPrefix + '/lib/UpdateStakeCurrenciesTable'),
   ChainAddressCache = require(rootPrefix + '/lib/cacheManagement/kitSaas/ChainAddress'),
-  contractConstants = require(rootPrefix + '/lib/globalConstant/contract');
+  SetupSimpleTokenBase = require(rootPrefix + '/tools/chainSetup/origin/simpleToken/Base'),
+  coreConstants = require(rootPrefix + '/config/coreConstants'),
+  logger = require(rootPrefix + '/lib/logger/customConsoleLogger'),
+  contractConstants = require(rootPrefix + '/lib/globalConstant/contract'),
+  chainAddressConstants = require(rootPrefix + '/lib/globalConstant/chainAddress'),
+  chainSetupConstants = require(rootPrefix + '/lib/globalConstant/chainSetupLogs');
 
 /**
+ * Class to deploy simple token.
  *
- * @class
+ * @class DeploySimpleToken
  */
 class DeploySimpleToken extends SetupSimpleTokenBase {
   /**
-   * Constructor
+   * Constructor to deploy simple token.
    *
-   * @param {Object} params
-   * @param {String} params.signerAddress - address who signs Tx
-   * @param {String} params.signerKey - private key of signerAddress
+   * @param {object} params
+   * @param {string} params.signerAddress: address who signs Tx
+   * @param {string} params.signerKey: private key of signerAddress
+   *
+   * @augments SetupSimpleTokenBase
    *
    * @constructor
    */
@@ -40,10 +43,9 @@ class DeploySimpleToken extends SetupSimpleTokenBase {
   }
 
   /**
-   * asyncPerform
+   * AsyncPerform
    *
    * @ignore
-   *
    * @return {Promise}
    */
   async asyncPerform() {
@@ -53,7 +55,7 @@ class DeploySimpleToken extends SetupSimpleTokenBase {
 
     oThis.addKeyToWallet();
 
-    let deployerResponse = await oThis._deployContract();
+    const deployerResponse = await oThis._deployContract();
 
     oThis.removeKeyFromWallet();
 
@@ -64,32 +66,31 @@ class DeploySimpleToken extends SetupSimpleTokenBase {
     return deployerResponse;
   }
 
-  /***
+  /**
+   * Deploy contract
    *
-   * deploy contract
-   *
-   * @return {Promise}
-   *
+   * @return {Promise<*>}
    * @private
    */
   async _deployContract() {
     const oThis = this;
 
-    let nonceRsp = await oThis.fetchNonce(oThis.signerAddress);
+    const nonceRsp = await oThis.fetchNonce(oThis.signerAddress);
 
-    let deployParams = {
+    const deployParams = {
       deployerAddr: oThis.signerAddress,
       gasPrice: oThis.gasPrice,
       gas: contractConstants.deploySimpleTokenGas,
       web3Provider: oThis.web3Instance,
       contractBin: CoreBins.simpleToken,
       contractAbi: CoreAbis.simpleToken,
-      nonce: nonceRsp.data['nonce']
+      nonce: nonceRsp.data.nonce
     };
 
-    let deployerObj = new DeployerKlass(deployParams),
+    const deployerObj = new DeployerKlass(deployParams),
       deployerResponse = await deployerObj.perform().catch(function(errorResponse) {
         logger.error(errorResponse);
+
         return errorResponse;
       });
 
@@ -103,21 +104,23 @@ class DeploySimpleToken extends SetupSimpleTokenBase {
     return deployerResponse;
   }
 
-  /***
+  /**
+   * Insert simple token contract address into chain address.
    *
-   * insert simple token contract address into chain address
+   * @param {object} deployerResponse
    *
-   * @return {Promise}
-   *
+   * @return {Promise<*>}
    * @private
    */
   async _insertIntoChainAddress(deployerResponse) {
     const oThis = this;
 
-    if (deployerResponse.isFailure()) return deployerResponse;
+    if (deployerResponse.isFailure()) {
+      return deployerResponse;
+    }
 
     await new ChainAddressModel().insertAddress({
-      address: deployerResponse.data['contractAddress'],
+      address: deployerResponse.data.contractAddress,
       associatedAuxChainId: 0,
       addressKind: chainAddressConstants.stContractKind,
       deployedChainId: oThis.configStrategyObject.originChainId,
