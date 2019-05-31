bbbe5ed4c10fa28a6d324c68a1e9814e79831839
AnaghaMurtarkar <anagha@ost.com>
2019-05-31 19:39:33 +0530

    Webhook pre processor subscriber.

--- app/models/mysql/PendingWebhook.js
+++ app/models/mysql/PendingWebhook.js
@@ -31,6 +31,44 @@ class PendingWebhook extends ModelBase {
 
     oThis.tableName = 'pending_webhooks';
   }
+
+  /**
+   * Inserts record.
+   *
+   * @param {Object} params
+   * @param {Number} params.clientId: client id
+   * @param {String} params.eventUuid: event uuid (uuid v4)
+   * @param {Number} params.topic: topic
+   * @param {Number} params.status: status
+   * @param {String} [params.extraData]: extra data
+   *
+   * @returns {Promise<*>}
+   */
+  async insertRecord(params) {
+    const oThis = this;
+
+    // Perform validations.
+    if (
+      !params.hasOwnProperty('clientId') ||
+      !params.hasOwnProperty('eventUuid') ||
+      !params.hasOwnProperty('topic') ||
+      !params.hasOwnProperty('status')
+    ) {
+      throw 'Mandatory parameters are missing. Expected an object with the following keys: {clientId, chainId, groupId, status}';
+    }
+
+    return oThis
+      .insert({
+        client_id: params.clientId,
+        event_uuid: params.eventUuid,
+        topic: params.topic,
+        extra_data: params.extraData || null,
+        status: params.status,
+        retry_count: 0,
+        last_attempted_at: Date.now() / 1000
+      })
+      .fire();
+  }
 }
 
 module.exports = PendingWebhook;
