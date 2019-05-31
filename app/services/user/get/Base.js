cc79b3b7ec5672c891042798aca1ea1aa5c733b1
shlokgilda <shlok@ost.com>
2019-05-31 18:07:53 +0530

    Comment changes.
    Signed-off-by: shlokgilda <shlok@ost.com>

--- app/services/user/get/Base.js
+++ app/services/user/get/Base.js
@@ -1,30 +1,39 @@
-'use strict';
 /**
- * Base service class for getting user
+ * Module for getting user.
  *
  * @module app/services/user/get/Base
  */
+
 const rootPrefix = '../../../..',
   ServiceBase = require(rootPrefix + '/app/services/Base'),
   coreConstants = require(rootPrefix + '/config/coreConstants'),
   responseHelper = require(rootPrefix + '/lib/formatter/response'),
   basicHelper = require(rootPrefix + '/helpers/basic');
 
-// Following require(s) for registering into instance composer
+// Following require(s) for registering into instance composer.
 require(rootPrefix + '/app/models/ddb/sharded/User');
 require(rootPrefix + '/lib/cacheManagement/chain/TokenShardNumber');
 require(rootPrefix + '/lib/cacheManagement/chainMulti/TokenUserDetail');
 
+/**
+ * Class for getting user.
+ *
+ * @class GetUserBase
+ */
 class GetUserBase extends ServiceBase {
   /**
-   * @constructor
+   * Constructor for getting user.
    *
-   * @param {Object} params
-   * @param {Number} params.client_id - client Id
-   * @param {Number} [params.token_id] - token Id
-   * @param {Array} [params.ids] - filter by user uuids.
-   * @param {Array} [params.limit] - limit
-   * @param {String} [params.pagination_identifier] - pagination identifier to fetch page
+   * @param {object} params
+   * @param {number} params.client_id: client Id
+   * @param {number} [params.token_id]: token Id
+   * @param {array} [params.ids]: filter by user uuids.
+   * @param {array} [params.limit]: limit
+   * @param {string} [params.pagination_identifier]: pagination identifier to fetch page
+   *
+   * @augments ServiceBase
+   *
+   * @constructor
    */
   constructor(params) {
     super(params);
@@ -39,9 +48,10 @@ class GetUserBase extends ServiceBase {
   }
 
   /**
-   * perform - Perform Get user lists
+   * Async perform: Perform Get user lists.
    *
    * @return {Promise<void>}
+   * @private
    */
   async _asyncPerform() {
     const oThis = this;
@@ -60,9 +70,9 @@ class GetUserBase extends ServiceBase {
   }
 
   /**
-   * _fetchTokenUsersShards - fetch token user shards from cache
+   * Fetch token user shards from cache.
    *
-   * Sets oThis.userShard
+   * @sets oThis.userShard
    *
    * @return {Promise<void>}
    * @private
@@ -70,12 +80,12 @@ class GetUserBase extends ServiceBase {
   async _fetchTokenUsersShards() {
     const oThis = this;
 
-    let TokenShardNumbersCache = oThis.ic().getShadowedClassFor(coreConstants.icNameSpace, 'TokenShardNumbersCache');
-    let tokenShardNumbersCache = new TokenShardNumbersCache({
+    const TokenShardNumbersCache = oThis.ic().getShadowedClassFor(coreConstants.icNameSpace, 'TokenShardNumbersCache');
+    const tokenShardNumbersCache = new TokenShardNumbersCache({
       tokenId: oThis.tokenId
     });
 
-    let response = await tokenShardNumbersCache.fetch();
+    const response = await tokenShardNumbersCache.fetch();
 
     if (!response || !response.data.user) {
       return Promise.reject(
@@ -93,19 +103,22 @@ class GetUserBase extends ServiceBase {
   /**
    * Fetch user details.
    *
-   * @return {Promise<string>}
+   * @sets oThis.userDetails
+   *
+   * @returns {Promise<void>}
+   * @private
    */
   async _fetchUsersFromCache() {
     const oThis = this,
       TokenUserDetailsCache = oThis.ic().getShadowedClassFor(coreConstants.icNameSpace, 'TokenUserDetailsCache'),
       tokenUserDetailsCacheObj = new TokenUserDetailsCache({ tokenId: oThis.tokenId, userIds: oThis.userIds });
 
-    let cacheResponse = await tokenUserDetailsCacheObj.fetch();
+    const cacheResponse = await tokenUserDetailsCacheObj.fetch();
 
     if (cacheResponse.isSuccess()) {
-      let usersData = cacheResponse.data;
-      for (let index in oThis.userIds) {
-        let uuid = oThis.userIds[index];
+      const usersData = cacheResponse.data;
+      for (const index in oThis.userIds) {
+        const uuid = oThis.userIds[index];
         if (!basicHelper.isEmptyObject(usersData[uuid])) {
           oThis.userDetails.push(usersData[uuid]);
         }
@@ -114,29 +127,33 @@ class GetUserBase extends ServiceBase {
   }
 
   /**
-   * Validate and samitnize params
+   * Validate and sanitize params.
    *
    * @return {Promise<void>}
    * @private
    */
   async _validateAndSanitizeParams() {
-    throw 'sub-class to implement';
+    throw new Error('Sub-class to implement.');
   }
 
   /**
-   * Set user ids
+   * Set user ids.
    *
+   * @return {Promise<void>}
    * @private
    */
   async _setUserIds() {
-    throw 'sub-class to implement';
+    throw new Error('Sub-class to implement.');
   }
 
   /**
+   * Format API response.
+   *
+   * @return {Promise<void>}
    * @private
    */
   async _formatApiResponse() {
-    throw 'sub-class to implement';
+    throw new Error('Sub-class to implement.');
   }
 }
 
