9986dfda2b558b8bf2b02f54fed35063c96aa6e8
shlokgilda <shlok@ost.com>
2019-06-10 12:29:14 +0530

    Comment changes.
    Signed-off-by: shlokgilda <shlok@ost.com>

--- app/services/device/multisigOperation/Base.js
+++ app/services/device/multisigOperation/Base.js
@@ -1,7 +1,5 @@
-'use strict';
-
 /**
- * Base for device related multi sig operations
+ * Module for base class for device related multi sig operations.
  *
  * @module app/services/device/multisigOperation/Base
  */
@@ -23,43 +21,48 @@ const rootPrefix = '../../../..',
   publishToPreProcessor = require(rootPrefix + '/lib/webhooks/publishToPreProcessor'),
   configStrategyConstants = require(rootPrefix + '/lib/globalConstant/configStrategy');
 
-// Following require(s) for registering into instance composer
+// Following require(s) for registering into instance composer.
 require(rootPrefix + '/lib/cacheManagement/chainMulti/TokenUserDetail');
 require(rootPrefix + '/lib/cacheManagement/chainMulti/DeviceDetail');
 require(rootPrefix + '/lib/device/UpdateStatus');
 require(rootPrefix + '/app/models/ddb/sharded/Device');
 
 /**
- * Class for device related multi sig operations
+ * Class for device related multi sig operations.
  *
  * @class MultisigOpertationBaseKlass
  */
 class MultisigOpertationBaseKlass extends ServiceBase {
   /**
+   * Constructor for device related multi sig operations.
    *
-   * @param {Object} params
-   * @param {Number} params.client_id
-   * @param {Number} params.token_id
-   * @param {Object} params.user_data
+   * @param {object} params
+   * @param {number} params.client_id
+   * @param {number} params.token_id
+   * @param {object} params.user_data
    *
-   * @param {String} params.to - Destination address of Safe transaction, multisig proxy address
-   * @param {String/Number} params.value - Ether value of Safe transaction, eth value in wei
-   * @param {String} params.calldata - Data payload of Safe transaction
-   * @param {Number} params.operation - Operation type of Safe transaction
-   * @param {String/Number} params.safe_tx_gas - Gas that should be used for the Safe transaction
-   * @param {String/Number} params.data_gas - Gas costs for data used to trigger the safe transaction and to pay the payment transfer
-   * @param {String/Number} params.gas_price - Gas price that should be used for the payment calculation
-   * @param {String} params.gas_token - Token address (or 0 if ETH) that is used for the payment
-   * @param {String} params.refund_receiver - Address of receiver of gas payment (or 0 if tx.origin)
-   * @param {String} params.signatures - Packed signature data ({bytes32 r}{bytes32 s}{uint8 v})
-   * @param {Array} params.signers - array of authorized device addresses who signed this transaction
-   * @param {String/Number} params.nonce - multisig contract nonce
+   * @param {string} params.to: Destination address of Safe transaction, multisig proxy address
+   * @param {string/number} params.value: Ether value of Safe transaction, eth value in wei
+   * @param {string} params.calldata: Data payload of Safe transaction
+   * @param {number} params.operation: Operation type of Safe transaction
+   * @param {string/number} params.safe_tx_gas: Gas that should be used for the Safe transaction
+   * @param {string/number} params.data_gas: Gas costs for data used to trigger the safe transaction and to pay the payment transfer
+   * @param {string/number} params.gas_price: Gas price that should be used for the payment calculation
+   * @param {string} params.gas_token: Token address (or 0 if ETH) that is used for the payment
+   * @param {string} params.refund_receiver: Address of receiver of gas payment (or 0 if tx.origin)
+   * @param {string} params.signatures: Packed signature data ({bytes32 r}{bytes32 s}{uint8 v})
+   * @param {array} params.signers: array of authorized device addresses who signed this transaction
+   * @param {string/number} params.nonce: multisig contract nonce
+   *
+   * @augments ServiceBase
    *
    * @constructor
    */
   constructor(params) {
     super(params);
+
     const oThis = this;
+
     oThis.clientId = params.client_id;
     oThis.tokenId = params.token_id;
     oThis.userData = params.user_data;
@@ -86,7 +89,7 @@ class MultisigOpertationBaseKlass extends ServiceBase {
   }
 
   /**
-   * async perform
+   * Async perform.
    *
    * @returns {Promise<void>}
    * @private
@@ -102,6 +105,7 @@ class MultisigOpertationBaseKlass extends ServiceBase {
   }
 
   /**
+   * Sanitize parameters.
    *
    * @returns {Promise<void>}
    * @private
@@ -132,11 +136,12 @@ class MultisigOpertationBaseKlass extends ServiceBase {
 
     oThis.signer = basicHelper.sanitizeAddress(oThis.signers[0]);
 
-    // Sanitize action specific params
+    // Sanitize action specific params.
     await oThis._sanitizeSpecificParams();
   }
 
   /**
+   * Perform common pre checks.
    * 1. status is activated in users table
    * 2. multisig is present for that user
    * 3. token holder is present for that user
@@ -148,9 +153,9 @@ class MultisigOpertationBaseKlass extends ServiceBase {
   async _performCommonPreChecks() {
     const oThis = this;
 
-    let tokenUserDetails = oThis.userData;
+    const tokenUserDetails = oThis.userData;
 
-    //Check if user is activated
+    // Check if user is activated.
     if (
       tokenUserDetails.status !== tokenUserConstants.activatedStatus ||
       !tokenUserDetails.multisigAddress ||
@@ -158,6 +163,7 @@ class MultisigOpertationBaseKlass extends ServiceBase {
       tokenUserDetails.kind !== tokenUserConstants.userKind
     ) {
       logger.error('Token user is not set properly');
+
       return Promise.reject(
         responseHelper.paramValidationError({
           internal_error_identifier: 'a_s_dm_mo_b_2',
@@ -168,9 +174,10 @@ class MultisigOpertationBaseKlass extends ServiceBase {
       );
     }
 
-    //check multisig address
+    // Check multisig address.
     if (oThis.multisigAddress !== oThis.to) {
       logger.error('Multisig address mismatch');
+
       return Promise.reject(
         responseHelper.paramValidationError({
           internal_error_identifier: 'a_s_dm_mo_b_3',
@@ -180,17 +187,17 @@ class MultisigOpertationBaseKlass extends ServiceBase {
         })
       );
     }
-    //Validates if the signatures provided is valid.
+    // Validates if the signatures provided is valid.
     await oThis._validateSignature();
 
-    // Perform action specific pre checks
+    // Perform action specific pre checks.
     await oThis._performSpecificPreChecks();
 
     return responseHelper.successWithData({});
   }
 
   /**
-   * Validate signature
+   * Validate signature.
    *
    * @returns {Promise<never>}
    * @private
@@ -198,7 +205,7 @@ class MultisigOpertationBaseKlass extends ServiceBase {
   async _validateSignature() {
     const oThis = this;
 
-    let gnosisSafeProxyInstance = new GnosisSafeHelper(oThis.multisigAddress, oThis._web3Instance),
+    const gnosisSafeProxyInstance = new GnosisSafeHelper(oThis.multisigAddress, oThis._web3Instance),
       safeTxData = gnosisSafeProxyInstance.getSafeTxData(
         oThis.multisigAddress,
         oThis.value,
@@ -212,7 +219,7 @@ class MultisigOpertationBaseKlass extends ServiceBase {
         oThis.nonce
       );
 
-    let verifySignRsp = await signatureVerification.validateSignature(
+    const verifySignRsp = await signatureVerification.validateSignature(
       safeTxData.getEIP712SignHash(),
       oThis.signatures,
       oThis.signer
@@ -231,30 +238,30 @@ class MultisigOpertationBaseKlass extends ServiceBase {
   }
 
   /**
-   * Return specific device details
+   * Return specific device details.
    *
-   * @param deviceAddresses
+   * @param {string} deviceAddresses
    *
    * @returns {Promise<*>}
-   *
    * @private
    */
   async _fetchDeviceDetails(deviceAddresses) {
     const oThis = this;
 
-    let paramsForDeviceDetailsCache = {
+    const paramsForDeviceDetailsCache = {
       userId: oThis.userId,
       walletAddresses: deviceAddresses,
       tokenId: oThis.tokenId,
       shardNumber: oThis.deviceShardNumber
     };
 
-    let DeviceDetailsKlass = oThis.ic().getShadowedClassFor(coreConstants.icNameSpace, 'DeviceDetailCache'),
+    const DeviceDetailsKlass = oThis.ic().getShadowedClassFor(coreConstants.icNameSpace, 'DeviceDetailCache'),
       deviceDetailsObj = new DeviceDetailsKlass(paramsForDeviceDetailsCache),
       deviceDetailsRsp = await deviceDetailsObj.fetch();
 
     if (deviceDetailsRsp.isFailure()) {
       logger.error('No data found for the provided wallet address');
+
       return Promise.reject(
         responseHelper.error({
           internal_error_identifier: 'a_s_dm_mo_b_5',
@@ -270,9 +277,9 @@ class MultisigOpertationBaseKlass extends ServiceBase {
   /**
    * This function updates the device status of the respective record in devices table.
    *
-   * @param {String} deviceAddress
-   * @param {String} initialDeviceStatus
-   * @param {String} finalDeviceStatus
+   * @param {string} deviceAddress
+   * @param {string} initialDeviceStatus
+   * @param {string} finalDeviceStatus
    *
    * @returns {Promise<*>}
    *
@@ -282,7 +289,7 @@ class MultisigOpertationBaseKlass extends ServiceBase {
     const oThis = this;
 
     logger.debug('**** Updating device status');
-    let paramsToUpdateDeviceStatus = {
+    const paramsToUpdateDeviceStatus = {
       chainId: oThis._configStrategyObject.auxChainId,
       shardNumber: oThis.deviceShardNumber,
       userId: oThis.userId,
@@ -290,7 +297,7 @@ class MultisigOpertationBaseKlass extends ServiceBase {
       initialStatus: initialDeviceStatus,
       finalStatus: finalDeviceStatus
     };
-    let UpdateDeviceStatusKlass = oThis.ic().getShadowedClassFor(coreConstants.icNameSpace, 'UpdateDeviceStatus'),
+    const UpdateDeviceStatusKlass = oThis.ic().getShadowedClassFor(coreConstants.icNameSpace, 'UpdateDeviceStatus'),
       updateDeviceStatusObj = new UpdateDeviceStatusKlass(paramsToUpdateDeviceStatus),
       updateDeviceStatusRsp = await updateDeviceStatusObj.perform();
 
@@ -308,43 +315,53 @@ class MultisigOpertationBaseKlass extends ServiceBase {
   }
 
   /**
-   * Get web3instance to interact with chain
+   * Get web3instance to interact with chain.
    *
-   * @return {Object}
+   * @sets oThis.web3InstanceObj
+   *
+   * @return {object}
    */
   get _web3Instance() {
     const oThis = this;
 
-    if (oThis.web3InstanceObj) return oThis.web3InstanceObj;
+    if (oThis.web3InstanceObj) {
+      return oThis.web3InstanceObj;
+    }
 
-    let chainEndPoint = oThis._configStrategyObject.auxChainWsProvider(configStrategyConstants.gethReadWrite);
+    const chainEndPoint = oThis._configStrategyObject.auxChainWsProvider(configStrategyConstants.gethReadWrite);
+
     oThis.web3InstanceObj = web3Provider.getInstance(chainEndPoint).web3WsProvider;
 
     return oThis.web3InstanceObj;
   }
 
   /**
-   * Object of config strategy class
+   * Object of config strategy class.
    *
-   * @return {Object}
+   * @sets oThis.configStrategyObj
+   *
+   * @return {object}
    */
   get _configStrategyObject() {
     const oThis = this;
 
-    if (oThis.configStrategyObj) return oThis.configStrategyObj;
+    if (oThis.configStrategyObj) {
+      return oThis.configStrategyObj;
+    }
 
     oThis.configStrategyObj = new ConfigStrategyObject(oThis._configStrategy);
 
     return oThis.configStrategyObj;
   }
 
-  /***
+  /**
    * Config strategy
    *
-   * @return {Object}
+   * @return {object}
    */
   get _configStrategy() {
     const oThis = this;
+
     return oThis.ic().configStrategy;
   }
 
@@ -369,15 +386,15 @@ class MultisigOpertationBaseKlass extends ServiceBase {
   }
 
   _sanitizeSpecificParams() {
-    throw 'sub-class to implement';
+    throw new Error('Sub-class to implement.');
   }
 
   async _performSpecificPreChecks() {
-    throw 'sub-class to implement';
+    throw new Error('Sub-class to implement.');
   }
 
   async _performOperation() {
-    throw 'sub-class to implement';
+    throw new Error('Sub-class to implement.');
   }
 }
 
