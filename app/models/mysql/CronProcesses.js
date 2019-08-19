e8af238feb87694e457b9e6a8d4e5baa695adf64
AnkitSingh <ankit@ost.com>
2019-08-20 00:15:25 +0530

    latest transaction data

--- app/models/mysql/CronProcesses.js
+++ app/models/mysql/CronProcesses.js
@@ -39,7 +39,8 @@ const dbName = 'saas_' + coreConstants.subEnvironment + '_' + coreConstants.envi
     '26': cronProcessesConstants.recoveryRequestsMonitor,
     '27': cronProcessesConstants.webhookPreprocessor,
     '28': cronProcessesConstants.webhookProcessor,
-    '29': cronProcessesConstants.webhookErrorHandler
+    '29': cronProcessesConstants.webhookErrorHandler,
+    '30': cronProcessesConstants.trackLatestTransaction
   },
   statuses = {
     '1': cronProcessesConstants.runningStatus,
