f3e165b964b02888b5e8f8edbe90de320c677f63
shlokgilda <shlok@ost.com>
2019-09-26 18:35:44 +0530

    Bug fixes.
    Signed-off-by: shlokgilda <shlok@ost.com>

--- executables/oneTimers/HornetVerification.js
+++ executables/oneTimers/HornetVerification.js
@@ -2,36 +2,63 @@ const OSTBase = require('@ostdotcom/base'),
   InstanceComposer = OSTBase.InstanceComposer;
 
 const rootPrefix = '../../',
-  coreConstants = require(rootPrefix + '/config/coreConstants'),
-  basicHelper = require(rootPrefix + '/helpers/basic'),
-  blockScannerProvider = require(rootPrefix + '/lib/providers/blockScanner'),
   TransactionMetaModel = require(rootPrefix + '/app/models/mysql/TransactionMeta'),
-  ConfigStrategyByChainId = require(rootPrefix + '/helpers/configStrategy/ByChainId.js');
+  ConfigStrategyByChainId = require(rootPrefix + '/helpers/configStrategy/ByChainId.js'),
+  basicHelper = require(rootPrefix + '/helpers/basic'),
+  coreConstants = require(rootPrefix + '/config/coreConstants'),
+  logger = require(rootPrefix + '/lib/logger/customConsoleLogger'),
+  blockScannerProvider = require(rootPrefix + '/lib/providers/blockScanner');
 
+// Following require(s) for registering into instance composer.
 require(rootPrefix + '/lib/cacheManagement/chain/UserSessionAddress');
 
+// Declare economy related variables.
 const tokenId = '1185',
-  clientId = '10267',
   companyUUUID = '0d91bb6f-7301-4bb8-8cf0-655e16de4f84',
   chainId = '197',
-  conversionFactor = 10,
-  startDate = new Date('2019, 06, 01'),
-  endDate = new Date('2019, 07, 01');
+  conversionFactor = 10;
 
-let totalTokenTransfers = 0,
+// Declare query related variables.
+const startDate = new Date('2019, 06, 01'),
+  endDate = new Date('2019, 07, 01'),
+  queryLimit = 10;
+
+let totalTransactions = 0,
   totalVolume = 0;
 
+/**
+ * Class to verify transaction amount and count for economies.
+ *
+ * @class HornetVerification
+ */
 class HornetVerification {
+  constructor() {
+    const oThis = this;
+
+    oThis.blockScannerObj = null;
+    oThis.totalVolume = null;
+    oThis.ic = null;
+    oThis.sessionAddresses = [];
+  }
+
   perform() {
     const oThis = this;
 
     return oThis.asyncPerform();
   }
 
+  /**
+   * Async perform.
+   *
+   * @sets oThis.blockScannerObj, oThis.totalVolume
+   *
+   * @returns {Promise<void>}
+   */
   async asyncPerform() {
     const oThis = this;
 
     oThis.blockScannerObj = await blockScannerProvider.getInstance([chainId]);
+    oThis.totalVolume = basicHelper.convertToBigNumber(0);
 
     await oThis.fetchConfigStrategy();
 
@@ -40,13 +67,27 @@ class HornetVerification {
     await oThis.getTransactionHashes();
   }
 
+  /**
+   * Fetch config strategy.
+   *
+   * @sets oThis.ic
+   *
+   * @returns {Promise<void>}
+   */
   async fetchConfigStrategy() {
     const oThis = this;
 
-    oThis.configStrategy = (await new ConfigStrategyByChainId(chainId).getComplete()).data;
-    oThis.ic = new InstanceComposer(oThis.configStrategy);
+    const configStrategy = (await new ConfigStrategyByChainId(chainId).getComplete()).data;
+    oThis.ic = new InstanceComposer(configStrategy);
   }
 
+  /**
+   * Fetch company session addresses.
+   *
+   * @sets oThis.sessionAddresses
+   *
+   * @returns {Promise<never>}
+   */
   async fetchCompanySessionAddress() {
     const oThis = this;
 
@@ -63,17 +104,20 @@ class HornetVerification {
     oThis.sessionAddresses = userSessionAddressCacheResp.data.addresses;
   }
 
+  /**
+   * Get transaction information in batches.
+   *
+   * @returns {Promise<void>}
+   */
   async getTransactionHashes() {
     const oThis = this;
-
-    const limit = 10;
 
     let page = 1,
       offset = null,
       moreDataPresent = true;
 
     while (moreDataPresent) {
-      offset = (page - 1) * limit;
+      offset = (page - 1) * queryLimit;
 
       const whereClause = [
         'token_id = ? AND status = ? AND receipt_status = ? AND session_address NOT IN (?) AND created_at >= ? AND created_at < ?',
@@ -88,81 +132,101 @@ class HornetVerification {
       const dbRows = await new TransactionMetaModel()
         .select('transaction_hash')
         .where(whereClause)
-        .limit(limit)
+        .limit(queryLimit)
         .offset(offset)
         .fire();
 
       if (dbRows.length === 0) {
         moreDataPresent = false;
       } else {
-        let transactionHashes = [];
-        for (let i = 0; i < dbRows.length; i++) {
-          transactionHashes.push(dbRows[i].transaction_hash);
+        const transactionHashes = [];
+        for (let index = 0; index < dbRows.length; index++) {
+          transactionHashes.push(dbRows[index].transaction_hash);
         }
-        console.log('transactionHashes---', transactionHashes);
-        //await oThis._fetchTransactionDetails(transactionHashes);
+        logger.log('transactionHashes---', transactionHashes);
+        await oThis._fetchTransactionDetails(transactionHashes);
+        logger.log('====oThis.totalVolume=====11111========', oThis.totalVolume.toString(10));
+        moreDataPresent = false;
       }
       page++;
     }
 
-    console.log('totalTokenTransfers--------', totalTokenTransfers);
+    logger.log('totalTransactions--------', totalTransactions);
+    logger.log('====oThis.totalVolume=====2222222========', oThis.totalVolume.toString(10));
   }
 
+  /**
+   * Fetch transaction details.
+   *
+   * @param {array<string>} transactionHashes
+   *
+   * @sets oThis.totalVolume
+   *
+   * @returns {Promise<void>}
+   * @private
+   */
   async _fetchTransactionDetails(transactionHashes) {
-    const oThis = this,
-      promiseArray = [];
-
-    const GetTransactionDetails = oThis.blockScannerObj.transaction.Get;
-
-    while (transactionHashes.length) {
-      const currentTransactionHashes = transactionHashes.splice(0, 100);
-
-      const getTransactionDetails = new GetTransactionDetails(chainId, currentTransactionHashes);
-
-      promiseArray.push(
-        getTransactionDetails.perform().catch(function() {
-          console.error('Error in get transaction service.');
-        })
-      );
-    }
-
-    const promiseArrayResponse = await Promise.all(promiseArray);
-
-    let transactionTransfersMap = await oThis._getTransferDetails(transactionHashes);
-
-    console.log('------transactionTransfersMap----', transactionTransfersMap);
-
-    for (let index = 0; index < promiseArrayResponse.length; index++) {
-      const transactionGetResponse = promiseArrayResponse[index],
-        transactionDetails = transactionGetResponse.data;
-
-      for (let i = 0; i < transactionHashes.length; i++) {
-        let transactionHash = transactionHashes[i];
-
-        if (transactionDetails[transactionHash] && transactionDetails[transactionHash].transactionStatus === '1') {
-          console.log('transactionDetails----', transactionDetails[transactionHash].transfers);
-          totalTokenTransfers = totalTokenTransfers + Number(transactionDetails[transactionHash].totalTokenTransfers);
-        }
-
-        let transferEventsMap = transactionTransfersMap[transactionHash];
-        let amountInOneTx = basicHelper.convertToBigNumber(0);
-        for (let eventIndex in transferEventsMap) {
-          let transferEvent = transferEventsMap[eventIndex],
-            currentAmount = basicHelper.convertToBigNumber(transferEvent.amount);
-          amountInOneTx = amountInOneTx.add(currentAmount);
-        }
-        totalVolume = basicHelper
-          .convertToNormalForPower(amountInOneTx, 18)
-          .div(basicHelper.convertToBigNumber(conversionFactor))
-          .toFixed(5);
-      }
-    }
-  }
-
-  async _getTransferDetails(transactionHashes) {
     const oThis = this;
 
-    let GetTransfer = oThis.blockScannerObj.transfer.GetAll,
+    const GetTransactionDetails = oThis.blockScannerObj.transaction.Get;
+    const getTransactionDetails = new GetTransactionDetails(chainId, transactionHashes);
+
+    const promiseArray = [getTransactionDetails.perform(), oThis.getTransferDetails(transactionHashes)];
+    const promiseArrayResponse = await Promise.all(promiseArray);
+
+    const transactionGetResponse = promiseArrayResponse[0];
+    const transactionTransfersMap = promiseArrayResponse[1];
+
+    const transactionDetails = transactionGetResponse.data;
+
+    let batchTotalVolume = basicHelper.convertToBigNumber(0);
+
+    for (let ind = 0; ind < transactionHashes.length; ind++) {
+      const transactionHash = transactionHashes[ind];
+
+      if (transactionDetails[transactionHash] && transactionDetails[transactionHash].transactionStatus === '1') {
+        totalTransactions += 1;
+      }
+
+      const transferEventsMap = transactionTransfersMap[transactionHash];
+      let transactionHashTotalAmount = basicHelper.convertToBigNumber(0);
+      for (const eventIndex in transferEventsMap) {
+        const transferEvent = transferEventsMap[eventIndex],
+          currentAmount = basicHelper.convertToBigNumber(transferEvent.amount);
+        logger.log('===currentAmount========', currentAmount.toString(10));
+        transactionHashTotalAmount = transactionHashTotalAmount.add(currentAmount);
+        logger.log('===transactionHashTotalAmount======111111==', transactionHashTotalAmount.toString(10));
+      }
+
+      logger.log('===transactionHashTotalAmount=====22222222===', transactionHashTotalAmount.toString(10));
+
+      batchTotalVolume = batchTotalVolume.add(transactionHashTotalAmount);
+
+      logger.log('===batchTotalVolume=====1111111===', transactionHashTotalAmount.toString(10));
+    }
+
+    logger.log('===batchTotalVolume=====22222222===', batchTotalVolume.toString(10));
+
+    totalVolume = basicHelper
+      .convertLowerUnitToNormal(batchTotalVolume, 18)
+      .div(basicHelper.convertToBigNumber(conversionFactor))
+      .toFixed(5);
+
+    logger.log('===totalVolume========', totalVolume.toString(10));
+
+    oThis.totalVolume = oThis.totalVolume.add(totalVolume);
+  }
+
+  /**
+   * Get transfer details for transaction hashes.
+   *
+   * @param {array<string>} transactionHashes
+   * @returns {Promise<*>}
+   */
+  async getTransferDetails(transactionHashes) {
+    const oThis = this;
+
+    const GetTransfer = oThis.blockScannerObj.transfer.GetAll,
       getTransfer = new GetTransfer(chainId, transactionHashes),
       getTransferResp = await getTransfer.perform();
 
