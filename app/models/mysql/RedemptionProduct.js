e8ac5d3f03548a41c636017ec5ee85d0902b3292
Dhananjay8 <dhananjay@ost.com>
2020-02-13 19:11:32 +0530

    model and global constants

--- app/models/mysql/RedemptionProduct.js
+++ app/models/mysql/RedemptionProduct.js
@@ -1,14 +1,15 @@
 /**
- * Module for redemption products model.
+ * Module for redemption product model.
  *
  * @module app/models/mysql/RedemptionProduct
  */
 
 const rootPrefix = '../../..',
   ModelBase = require(rootPrefix + '/app/models/mysql/Base'),
+  basicHelper = require(rootPrefix + '/helpers/basic'),
   coreConstants = require(rootPrefix + '/config/coreConstants'),
   responseHelper = require(rootPrefix + '/lib/formatter/response'),
-  redemptionProductConstants = require(rootPrefix + '/lib/globalConstant/redemptionProduct');
+  redemptionProductsConstants = require(rootPrefix + '/lib/globalConstant/redemptionProducts');
 
 // Declare variables.
 const dbName = 'saas_' + coreConstants.subEnvironment + '_' + coreConstants.environment;
@@ -35,6 +36,37 @@ class RedemptionProduct extends ModelBase {
   }
 
   /**
+   * Format Db data.
+   *
+   * @param {object} dbRow
+   * @param {number} dbRow.id
+   * @param {string} dbRow.name
+   * @param {string} dbRow.description
+   * @param {Object} dbRow.image
+   * @param {Object} dbRow.denomination
+   * @param {string} dbRow.expiry_in_days
+   * @param {string} dbRow.status
+   * @param {string} dbRow.created_at
+   * @param {string} dbRow.updated_at
+   *
+   * @return {object}
+   * @private
+   */
+  static _formatDbData(dbRow) {
+    return {
+      id: dbRow.id,
+      name: dbRow.name,
+      description: dbRow.description,
+      image: JSON.parse(dbRow.image) || null,
+      denomination: JSON.parse(dbRow.denomination),
+      expiryInDays: dbRow.expiry_in_days,
+      status: redemptionProductsConstants.statuses[dbRow.status],
+      createdAt: dbRow.created_at,
+      updatedTimestamp: basicHelper.dateToSecondsTimestamp(dbRow.updated_at)
+    };
+  }
+
+  /**
    * Insert record.
    *
    * @param params
@@ -55,6 +87,33 @@ class RedemptionProduct extends ModelBase {
       .fire();
 
     return responseHelper.successWithData(insertResponse.insertId);
+  }
+
+  /**
+   * Fetch redemption product details by ids.
+   *
+   * @param {array<string/number>} ids
+   *
+   * @return {Promise<any>}
+   */
+  async fetchRedemptionProductsByIds(ids) {
+    const oThis = this,
+      response = {};
+
+    const dbRows = await oThis
+      .select('*')
+      .where(['id IN (?)', ids])
+      .fire();
+
+    if (dbRows.length === 0) {
+      return Promise.reject(new Error(`No entries found for ids: ${ids}.`));
+    }
+
+    for (let index = 0; index < dbRows.length; index++) {
+      response[dbRows[index].id] = RedemptionProduct._formatDbData(dbRows[index]);
+    }
+
+    return responseHelper.successWithData(response);
   }
 
   /**
---
+++ app/models/mysql/TokenRedemptionProduct.js
@@ -0,0 +1,133 @@
+/**
+ * Module for token redemption product model.
+ *
+ * @module app/models/mysql/TokenRedemptionProduct
+ */
+
+const rootPrefix = '../../..',
+  ModelBase = require(rootPrefix + '/app/models/mysql/Base'),
+  basicHelper = require(rootPrefix + '/helpers/basic'),
+  coreConstants = require(rootPrefix + '/config/coreConstants'),
+  responseHelper = require(rootPrefix + '/lib/formatter/response'),
+  tokenRedemptionProductsConstants = require(rootPrefix + '/lib/globalConstant/tokenRedemptionProducts');
+
+// Declare variables.
+const dbName = 'saas_' + coreConstants.subEnvironment + '_' + coreConstants.environment;
+
+/**
+ * Class for token redemption product model.
+ *
+ * @class TokenRedemptionProduct
+ */
+class TokenRedemptionProduct extends ModelBase {
+  /**
+   * Constructor for token redemption product model.
+   *
+   * @augments ModelBase
+   *
+   * @constructor
+   */
+  constructor() {
+    super({ dbName: dbName });
+
+    const oThis = this;
+
+    oThis.tableName = 'token_redemption_products';
+  }
+
+  /**
+   * Format Db data.
+   *
+   * @param {object} dbRow
+   * @param {number} dbRow.id
+   * @param {number} dbRow.token_id
+   * @param {number} dbRow.redemption_product_id
+   * @param {string} dbRow.name
+   * @param {string} dbRow.description
+   * @param {Object} dbRow.image
+   * @param {number} dbRow.sequence_number
+   * @param {string} dbRow.status
+   * @param {string} dbRow.created_at
+   * @param {string} dbRow.updated_at
+   *
+   * @return {object}
+   * @private
+   */
+  static _formatDbData(dbRow) {
+    return {
+      id: dbRow.id,
+      tokenId: dbRow.token_id,
+      redemptionProductId: dbRow.redemption_product_id,
+      name: dbRow.name || null,
+      description: dbRow.description || null,
+      image: JSON.parse(dbRow.image) || null,
+      sequenceNumber: dbRow.sequence_number,
+      status: tokenRedemptionProductsConstants.statuses[dbRow.status],
+      createdAt: dbRow.created_at,
+      updatedTimestamp: basicHelper.dateToSecondsTimestamp(dbRow.updated_at)
+    };
+  }
+
+  /**
+   * Fetch redemption product details by ids.
+   *
+   * @param {array<string/number>} ids
+   *
+   * @return {Promise<any>}
+   */
+  async fetchTokenRedemptionProductsByIds(ids) {
+    const oThis = this,
+      response = {};
+
+    const dbRows = await oThis
+      .select('*')
+      .where([
+        ' id IN (?) AND status = ?',
+        ids,
+        tokenRedemptionProductsConstants.invertedStatuses[tokenRedemptionProductsConstants.activeStatus]
+      ])
+      .fire();
+
+    if (dbRows.length === 0) {
+      return Promise.reject(new Error(`No entries found for ids: ${ids}.`));
+    }
+
+    for (let index = 0; index < dbRows.length; index++) {
+      response[dbRows[index].id] = TokenRedemptionProduct._formatDbData(dbRows[index]);
+    }
+
+    return responseHelper.successWithData(response);
+  }
+
+  /**
+   * Fetch redemption product ids by token id.
+   *
+   * @param tokenId
+   * @returns {Promise<*|result>}
+   */
+  async fetchProductIdsByTokenId(tokenId) {
+    const oThis = this,
+      productIds = [];
+
+    const dbRows = await oThis
+      .select('id')
+      .where([
+        'token_id = ? AND status = ?',
+        tokenId,
+        tokenRedemptionProductsConstants.invertedStatuses[tokenRedemptionProductsConstants.activeStatus]
+      ])
+      .fire();
+
+    if (dbRows.length === 0) {
+      return Promise.reject(new Error(`No entries found for token id: ${tokenId}.`));
+    }
+
+    for (let index = 0; index < dbRows.length; index++) {
+      productIds.push(dbRows[index].id);
+    }
+
+    return responseHelper.successWithData(productIds);
+  }
+}
+
+module.exports = TokenRedemptionProduct;
