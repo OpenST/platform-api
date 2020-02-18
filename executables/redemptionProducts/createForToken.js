eb46533fff7f876d68b72544d6239af9a463713a
AnaghaMurtarkar <murtarkaranagha@gmail.com>
2020-02-18 12:25:23 +0530

    Create redemption products for token script.

--- executables/redemptionProducts/createForToken.js
+++ executables/redemptionProducts/createForToken.js
@@ -1,8 +1,15 @@
 const program = require('commander');
 
+const OSTBase = require('@ostdotcom/base'),
+  InstanceComposer = OSTBase.InstanceComposer;
+
 const rootPrefix = '../..',
-  CreateTokenRedemptionProductsLib = require(rootPrefix + '/lib/redemption/products/CreateForToken'),
-  logger = require(rootPrefix + '/lib/logger/customConsoleLogger');
+  ConfigStrategyByChainId = require(rootPrefix + '/helpers/configStrategy/ByChainId'),
+  TokenByTokenId = require(rootPrefix + '/lib/cacheManagement/kitSaas/TokenByTokenId'),
+  ClientConfigGroupCache = require(rootPrefix + '/lib/cacheManagement/shared/ClientConfigGroup'),
+  coreConstants = require(rootPrefix + '/config/coreConstants'),
+  logger = require(rootPrefix + '/lib/logger/customConsoleLogger'),
+  responseHelper = require(rootPrefix + '/lib/formatter/response');
 
 program
   .option(
@@ -31,6 +38,8 @@ if (!program.tokenRedemptionProductId) {
   }
 }
 
+require(rootPrefix + '/lib/redemption/products/CreateForToken');
+
 class CreateTokenRedemptionProducts {
   constructor(params) {
     const oThis = this;
@@ -43,6 +52,89 @@ class CreateTokenRedemptionProducts {
 
   async perform() {
     const oThis = this;
+
+    await oThis._fetchClientIdByTokenId();
+
+    await oThis._fetchChainId();
+
+    await oThis.fetchConfigStrategy();
+
+    await oThis._createTokenRedemptionProducts();
+  }
+
+  /**
+   * This function fetches client id using token id.
+   *
+   * @returns {Promise<never>}
+   * @private
+   */
+  async _fetchClientIdByTokenId() {
+    const oThis = this;
+
+    const cacheResponse = await new TokenByTokenId({ tokenId: oThis.tokenId }).fetch();
+
+    if (cacheResponse.isFailure() || !cacheResponse.data) {
+      return Promise.reject(
+        // This is not a param validation error because we are fetching token id and/or client id internally.
+        responseHelper.error({
+          internal_error_identifier: 'e_rp_cft_1',
+          api_error_identifier: 'something_went_wrong',
+          debug_options: {}
+        })
+      );
+    }
+
+    oThis.clientId = cacheResponse.data.clientId;
+  }
+
+  /**
+   * Fetch client config strategy.
+   *
+   * @sets oThis.auxChainId
+   *
+   * @returns {Promise<*>}
+   * @private
+   */
+  async _fetchChainId() {
+    const oThis = this;
+
+    // Fetch client config group.
+    const fetchCacheRsp = await new ClientConfigGroupCache({ clientId: oThis.clientId }).fetch();
+
+    if (fetchCacheRsp.isFailure()) {
+      return Promise.reject(fetchCacheRsp);
+    }
+
+    oThis.auxChainId = fetchCacheRsp.data[oThis.clientId].chainId;
+  }
+
+  /**
+   * Fetch config strategy.
+   *
+   * @sets oThis.ic
+   *
+   * @returns {Promise<void>}
+   */
+  async fetchConfigStrategy() {
+    const oThis = this;
+
+    const configStrategy = (await new ConfigStrategyByChainId(oThis.auxChainId).getComplete()).data;
+    oThis.ic = new InstanceComposer(configStrategy);
+  }
+
+  /**
+   * Create token redemption products.
+   *
+   * @returns {Promise<void>}
+   * @private
+   */
+  async _createTokenRedemptionProducts() {
+    const oThis = this;
+
+    const CreateTokenRedemptionProductsLib = oThis.ic.getShadowedClassFor(
+      coreConstants.icNameSpace,
+      'CreateTokenRedemptionProducts'
+    );
 
     await new CreateTokenRedemptionProductsLib({
       tokenId: oThis.tokenId,
